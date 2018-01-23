d615ea0d57af12f3348a8e8fccf6790df7a82754
Kristopher Borel <k.borel3@gmail.com>
2018-01-24 10:30:03 +1100

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import marked from 'marked';
 
-import InputArea from './components/input-area';
-
-
 class App extends Component {
   constructor(props) {
     super(props);
