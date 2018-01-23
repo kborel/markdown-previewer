9851cb3824c889cbe723c97c54109f9b5c373732
Kristopher Borel <k.borel3@gmail.com>
2018-01-23 20:06:56 +1100

    Refactor project

--- src/App.js
+++ src/App.js
@@ -10,32 +10,37 @@ class App extends Component {
     super(props);
 
     this.state = {
-      value: '',
-      markup: ''
+      text: '',
     }
   }
 
-  markupText = (value) => {
+  markupText = text =>
     this.setState({
-      value: value
+      text,
     });
-  };
 
   createMarkup = () => {
-    return {__html: marked(this.state.value)};
+    return {__html: marked(this.state.text)};
   };
 
   render() {
     return (
-      <div className="row">
-        <InputArea 
-          className="col-md-6"
-          value={this.state.value}
-          onTextChange={(value) => this.markupText(value)}
-        />
-        <div 
-          className="col-md-6"
-          dangerouslySetInnerHTML={this.createMarkup()}/>
+      <div className="App">
+        <div className="App-header">
+          <h1>Markdown Previewer</h1>
+        </div>
+        <div className="content">
+          <textarea
+            placeholder="Enter markdown here..."
+            value={this.state.text}
+            className="input" 
+            onChange={event => this.markupText(event.target.value)}
+          />
+          <div
+            className="output" 
+            dangerouslySetInnerHTML={this.createMarkup()}
+          />
+        </div>
       </div>
     );
   }
